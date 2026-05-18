c75a099e71cb33d11f6c32a75f0730885d2e12fd
Your Name <your.email@example.com>
2026-05-18 08:53:55 +0000

    3.3

--- src/main/java/com/book/web/BookController.java
+++ src/main/java/com/book/web/BookController.java
@@ -2,12 +2,11 @@ package com.book.web;
 
 import com.book.domain.Book;
 import com.book.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.jws.WebParam;
 import javax.jws.WebParam;
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -159,17 +158,17 @@ public class BookController {
         return modelAndView;
     }
 
-
-
-    @RequestMapping("/readerbookdetail.html")
-    public ModelAndView readerBookDetail(HttpServletRequest request){
-        long bookId=Integer.parseInt(request.getParameter("bookId"));
-        Book book=bookService.getBook(bookId);
-        ModelAndView modelAndView=new ModelAndView("reader_book_detail");
-        modelAndView.addObject("detail",book);
-        return modelAndView;
+    @RequestMapping("/api/book/detail")
+    public String apiBookDetail(HttpServletRequest request) {
+        try {
+            long bookId = Long.parseLong(request.getParameter("bookId"));
+            Book book = bookService.getBook(bookId);
+            com.fasterxml.jackson.databind.ObjectMapper mapper = new com.fasterxml.jackson.databind.ObjectMapper();
+            return mapper.writeValueAsString(book);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "{}";
+        }
     }
-
-
 
 }
